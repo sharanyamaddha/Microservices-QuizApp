330fca85048a97d3f188dd598935a5f1f7417467
Sharanya Maddha <sharanyamaddha@gmail.com>
2025-11-28 23:40:14 +0530

    added quizApp

---
+++ src/main/java/com/quizapp/model/Response.java
@@ -0,0 +1,10 @@
+package com.quizapp.model;
+
+import lombok.Data;
+
+@Data
+public class Response {
+	private Integer id;
+	private String response;
+
+}
---
+++ src/main/java/com/quizapp/service/QuestionService.java
@@ -0,0 +1,52 @@
+package com.quizapp.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.quizapp.model.Question;
+import com.quizapp.repository.QuestionRepository;
+
+@Service
+public class QuestionService {
+	
+
+	@Autowired
+	QuestionRepository questionRepository;
+	
+	public ResponseEntity<List<Question>> getAllQuestions() {
+		try {
+		return new ResponseEntity(questionRepository.findAll(),HttpStatus.OK);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(new ArrayList(),HttpStatus.BAD_REQUEST);
+
+	}
+
+	public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+		try {
+			return new ResponseEntity(questionRepository.findByCategory(category),HttpStatus.OK);
+		}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+			return new ResponseEntity<>(new ArrayList(),HttpStatus.BAD_REQUEST);
+
+		
+	}
+
+	public ResponseEntity<String> addQuestion(Question question) {
+		questionRepository.save(question);
+		
+		return new ResponseEntity<>("success",HttpStatus.CREATED);
+	}
+
+	
+	
+}
